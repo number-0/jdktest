65d7795448dbb615d37fb1fe64c6147674309b35
songhengliang <xingren123>
2019-08-26 15:44:33 +0800

    jdk-test

--- src/main/java/com/shl/jdktest/myratelimiter/guava/RateLimiterDemo.java
+++ src/main/java/com/shl/jdktest/myratelimiter/guava/RateLimiterDemo.java
@@ -14,6 +14,8 @@ public class RateLimiterDemo {
     /**
      * RateLimiter#create(double permitsPerSecond)：每秒钟允许多少个许可
      * 0.5：1秒钟允许有0.5个许可，那就是2秒钟获取1个许可
+     *
+     * RateLimiter#acquire()：匀速获取许可，会阻塞再没有到达获取时间的时候，因为是匀速
      */
     private static final RateLimiter RATE_LIMITER = RateLimiter.create(0.5);
 
